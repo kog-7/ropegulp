98940d38cf60575c54b450f8cc98b20dc33d6b72
kog-7 <443536402@qq.com>
2018-04-29 23:03:38 +0800

    chang bin

--- bin/ropegulp.js
+++ bin/ropegulp.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-let data=require("./src/data.js");
-let bootstrap=require("./src/bootstrap.js");
+let data=require("../src/data.js");
+let bootstrap=require("../src/bootstrap.js");
 let args=Array.from(process.argv).slice(2);
 data.init(args);//同步的
 bootstrap();
